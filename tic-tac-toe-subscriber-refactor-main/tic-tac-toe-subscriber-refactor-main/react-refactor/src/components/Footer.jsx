a792295dc9b320a2946719473f0c42120d12db9e
NENAVATH KALYAN <161618055+kalyan73311@users.noreply.github.com>
2025-09-01 23:45:46 +0530

    Add files via upload

---
+++ tic-tac-toe-subscriber-refactor-main/tic-tac-toe-subscriber-refactor-main/react-refactor/src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        Original project by
+        <a href="https://twitter.com">@KALYAN</a> and
+        <a href="https://twitter.com">@KALYAN</a>
+      </p>
+
+      <p>
+        Refactored by
+        <a
+          href="https://twitter.com/"
+          style={{ color: "var(--turquoise)" }}
+        >
+          @zg_dev
+        </a>
+      </p>
+    </footer>
+  );
+}
